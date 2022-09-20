c3267562dff845a984e8dfe0132f4bb240cb5747
zyzz <zyzz5@gmail.co>
2022-09-20 12:55:54 -0500

    1.0.1 primera prueba

---
+++ src/main/java/com/mycompany/classone/ClassOne.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.classone;
+
+/**
+ *
+ * @author sala8
+ */
+public class ClassOne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Persona p= new Persona("Santiago Cabanzo",1234,"santiago@"); 
+        System.out.println(" objeto p"+p);
+        Math.sqrt(25);
+    }
+}
